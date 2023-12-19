4810171a3eb4ff7a19018fb65ed6c9b8e13ef149
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-12-19 12:37:21 +0100

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -8,3 +8,10 @@ function bubbles() {
           let x = Math.floor(Math.random() * window.innerWidth);
           let y = Math.floor(Math.random() * window.innerHeight);
   
+  let size = Math.random() * 10;
+        bubble.style.left = x + 'px';
+        bubble.style.top = y + 'px';      
+        bubble.style.width = 1 + size + 'px';
+        bubble.style.height = 1 + size + 'px';
+
+        bubble.style.animationDuration = 6 + size + 's';
