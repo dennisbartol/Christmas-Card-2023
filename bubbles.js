647bd6c1137e77e22ed656cf2857c289066616d9
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-12-19 12:16:00 +0100

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -2,3 +2,9 @@ function bubbles() {
      let count = 200;
      let section = document.querySelector('section');
      let i = 0; 
+
+  while (i < count) {
+          let bubble = document.creatElement('i');
+          let x = Math.floor(Math.random() * window.innerWidth);
+          let y = Math.floor(Math.random() * window.innerHeight);
+  
